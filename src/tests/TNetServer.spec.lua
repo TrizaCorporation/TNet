b47e49c16c7a713431bfbe86f81774d2160d24a5
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2023-02-27 15:02:37 +0000

    Add More Server Tests

--- src/tests/TNetServer.spec.lua
+++ src/tests/TNetServer.spec.lua
@@ -11,5 +11,25 @@ return function ()
         it("should have middleware as an empty table", function()
             expect(#TNet.Middleware == 0).to.be.ok()
         end)
+
+        local RemoteHandler
+
+        it("should handle a new event", function()
+            local Event = Instance.new("RemoteEvent")
+
+            local success = pcall(function()
+                RemoteHandler = TNet:HandleRemoteEvent(Event)
+            end)
+
+            expect(success).to.be.ok()
+        end)
+
+        it("should error on no client", function()
+            local success = pcall(function()
+                RemoteHandler:Fire()
+            end)
+
+            expect(not success).to.be.ok()
+        end)
     end)
 end
